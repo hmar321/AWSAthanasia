d8533d155252f1dfa5675a543518f0d520153f8e
Jhonny0102 <fjhon868@gmail.com>
2024-05-30 09:49:27 +0200

    jueves mañana

---
+++ Athanasia/Helpers/HelperCacheRedis.cs
@@ -0,0 +1,23 @@
+﻿using StackExchange.Redis;
+
+namespace Athanasia.Helpers
+{
+    public class HelperCacheRedis
+    {
+        private static Lazy<ConnectionMultiplexer> CreateConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            //AQUI IRA NUESTRA CADENA DE CONECCION
+            string connectionString = "cache-proyecto-aws.uk9dp8.ng.0001.use1.cache.amazonaws.com:6379";
+            return ConnectionMultiplexer.Connect(connectionString);
+        });
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return CreateConnection.Value;
+            }
+        }
+    }
+}
+}
